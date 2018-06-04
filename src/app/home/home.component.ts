e6a62816a96016e72f1f507eb515d092bd60d4da
MartinCarpentier <martin@carpentier.dk>
2018-06-04 22:13:01 +0200

    Added alot of changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Technology } from '../models/technology';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+    martinProfile: string;
+    kamstrupImage: string;
 
-  constructor() { }
+    technology: Technology[] = [
+        { id: 13, name: 'WPF Development', wanted : '0%', current : '80%'  },
+        { id: 14, name: 'Java programming', wanted : '20%', current : '65%'  },
+        { id: 12, name: 'Android Development', wanted : '30%', current : '70%'  },
+        { id: 15, name: 'SQL programming', wanted : '10%', current : '50%'  }
+      ];
 
-  ngOnInit() {
-  }
+    constructor() {
+        this.kamstrupImage = '/assets/img/Kamstrup_logo_red_small.jpg';
+        this.martinProfile = '/assets/img/mig2.jpg';
+    }
 
+    ngOnInit() {
+    }
 }
