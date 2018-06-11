31de59df8f8a7d8bcb7d47802edff84c6d08c050
MartinCarpentier <martin@carpentier.dk>
2018-06-11 22:38:00 +0200

    Made changes to hashtester component related to the gui. Calculating hashes is still not possible

--- src/app/hashtester/hashtester.component.ts
+++ src/app/hashtester/hashtester.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-hashtester',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hashtester.component.css']
 })
 export class HashtesterComponent implements OnInit {
+    arrow: string;
+    smallerThanMd: boolean;
+    hashAlgorithm: string;
 
-  constructor() { }
+  constructor() {
+    this.arrow = '/assets/img/Sideways_Arrow_Icon.svg';
+    this.smallerThanMd = window.innerWidth <= 767;
+   }
 
   ngOnInit() {
+    this.hashAlgorithm = 'sha1';
   }
 
+  onResize(event) {
+    this.smallerThanMd = event.target.innerWidth <= 767;
+    console.log("Current window size is "+ event.target.innerWidth);
+    console.log("smallerThanMdIs "+ this.smallerThanMd);
+  }
 }
